667dae0d0d086bc52149d9d8338fa9ce109be6c9
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-11-18 11:09:11 -0500

    Adding Vue.js Calendar

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <Calendar />
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import Calendar from './components/Calendar';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    Calendar
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
+
+import VueTextareaAutosize from 'vue-textarea-autosize';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiuo7EwR2FWU8WDJoeNhumDd8q_l9_ISc",
+  authDomain: "calendar-bded1.firebaseapp.com",
+  databaseURL: "https://calendar-bded1.firebaseio.com",
+  projectId: "calendar-bded1",
+  storageBucket: "calendar-bded1.appspot.com",
+  messagingSenderId: "599252354914",
+  appId: "1:599252354914:web:87af747345a52ed476ce3b",
+  measurementId: "G-00ZRRVJEQ7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
